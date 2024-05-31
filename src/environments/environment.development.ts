d1af4a15013915fb1006659c86be1a26691735f6
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-05-31 19:17:13 +0200

    refactor: Update environment.development.ts with Supabase configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://epprdiwjgfgdwtkgtfps.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwcHJkaXdqZ2ZnZHd0a2d0ZnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNjgzMDMsImV4cCI6MjAzMjc0NDMwM30.cK_0CJ6W2dt3_LjwqpnCQ-dm1S60a45pKjZvMIshOho',
+};
